1a79f276c6a25033f3a180b1079034c8cfd70a81
wzhao <cts-wzhao@live.com>
2015-01-09 10:17:41 +0800

    update configure

--- Includes/simplecms-config.php
+++ Includes/simplecms-config.php
@@ -1,11 +1,11 @@
 <?php
-    define('DB_NAME', 'wzhaoMySQLTest');
-    define('DB_USER', 'd57deed3857cea');
-    define('DB_PASSWORD', 'c3405729');
-    define('DB_HOST', 'us-cdbr-azure-east-a.cloudapp.net');
+    define('DB_NAME', 'db name');
+    define('DB_USER', 'db user');
+    define('DB_PASSWORD', 'db password');
+    define('DB_HOST', 'server name');
 
     define('DEFAULT_ADMIN_USERNAME', 'admin');
-    define('DEFAULT_ADMIN_PASSWORD', 'TdKxl520');
+    define('DEFAULT_ADMIN_PASSWORD', '@ntresR0cks');
 
     //Database=wzhaoMySQLTest;Data Source=us-cdbr-azure-east-a.cloudapp.net;User Id=d57deed3857cea;Password=c3405729
 ?>
